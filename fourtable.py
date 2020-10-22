ff1393c1c16678145f9ce38d5bd71203fa148097
itsTurnip <22935812+itsTurnip@users.noreply.github.com>
2020-10-22 23:35:02 +0500

    Переписанный шифр четырех квадратов

--- fourtable.py
+++ fourtable.py
@@ -11,9 +11,7 @@ class Four(Table):
     def __init__(self, k1: str, k2: str):
         k1, k2 = map(self.replace, [k1, k2])
         if self.check_bad_lang(k1, k2):
-            print("Должны использоваться исключительно русские символы!")
-            del self
-            return
+            raise ValueError("Должны использоваться исключительно русские символы!")
         k1, k2 = self.edit_key(k1), self.edit_key(k2)
         self.alph1, self.alph2 = self.make_alph(k1), self.make_alph(k2)
 
@@ -24,34 +22,11 @@ class Four(Table):
         :param bigr: шифруемая биграмма
         :returns: зашифрованная биграмма
         """
-        nums = [[self.alphabet.index(bigr[0]) // 5, self.alphabet.index(bigr[0]) % 5],
-                [self.alphabet.index(bigr[1]) // 5, self.alphabet.index(bigr[1]) % 5]]
-        """Массив для хранения строк и столбцов символов биграммы"""
+        first_x, first_y = self.alphabet.index(bigr[0]) // 5, self.alphabet.index(bigr[0]) % 5
+        second_x, second_y = self.alphabet.index(bigr[1]) // 5, self.alphabet.index(bigr[1]) % 5
         # Меняем столбцы местами
-        t = nums[0][1]
-        nums[0][1] = nums[1][1]
-        nums[1][1] = t
-        # Формируем зашифрованную биграмму
-        bigr = "".join([self.alph1[nums[0][0] * 5 + nums[0][1]], self.alph2[nums[1][0] * 5 + nums[1][1]]])
-        return bigr
-
-    def crypt(self, text: str) -> str:
-        """
-        Функция шифрования текста
-
-        :param text: шифруемый текст
-        :returns: зашифрованный текст
-        """
-        text = self.replace(text)
-        if self.check_bad_lang(text):
-            print("Допускается использовать только русский алфавит!")
-            return
-        if len(text) % 2 == 1:
-            text += "ь"
-        ctext = ""
-        while len(ctext) < len(text):
-            ctext += self.crypt_b(text[len(ctext)] + text[len(ctext) + 1])
-        return ctext
+        first_y, second_y = second_y, first_y
+        return self.alph1[first_x * 5 + first_y] + self.alph2[second_x * 5 + second_y]
 
     def decrypt_b(self, bigr: str) -> str:
         """
@@ -60,25 +35,9 @@ class Four(Table):
         :param bigr: биграмма для расшифрования
         :return: расшифрованная биграмма
         """
-        nums = [[self.alph1.index(bigr[0]) // 5, self.alph1.index(bigr[0]) % 5],
-                [self.alph2.index(bigr[1]) // 5, self.alph2.index(bigr[1]) % 5]]
+        first_x, first_y = self.alph1.index(bigr[0]) // 5, self.alph1.index(bigr[0]) % 5
+        second_x, second_y = self.alph2.index(bigr[1]) // 5, self.alph2.index(bigr[1]) % 5
         """Массив для хранения строк и столбцов символов биграммы"""
         # Меняем столбцы местами
-        t = nums[0][1]
-        nums[0][1] = nums[1][1]
-        nums[1][1] = t
-        # Формируем зашифрованную биграмму
-        bigr = "".join([self.alphabet[nums[0][0] * 5 + nums[0][1]], self.alphabet[nums[1][0] * 5 + nums[1][1]]])
-        return bigr
-
-    def decrypt(self, text: str) -> str:
-        """
-        Функция расшифрования текста
-
-        :param text: шифруемый текст
-        :returns: зашифрованный текст
-        """
-        ctext = ""
-        while len(ctext) < len(text):
-            ctext += self.decrypt_b(text[len(ctext)] + text[len(ctext) + 1])
-        return ctext
+        first_y, second_y = second_y, first_y
+        return self.alphabet[first_x * 5 + first_y] + self.alphabet[second_x * 5 + second_y]
