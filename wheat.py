b505b18617fa6e4c2af9600f0c46ce08d72d3f89
itsTurnip <22935812+itsTurnip@users.noreply.github.com>
2020-10-22 23:22:52 +0500

    Фикс Уитстона

--- wheat.py
+++ wheat.py
@@ -11,56 +11,45 @@ class Wheat(Table):
     def __init__(self, k1: str, k2: str):
         k1, k2 = map(self.replace, [k1, k2])
         if self.check_bad_lang(k1, k2):
-            print("Должны использоваться исключительно русские символы!")
-            del self
-            return
+            raise ValueError("Должны использоваться только русские символы в качетсве ключей")
         k1, k2 = self.edit_key(k1), self.edit_key(k2)
         self.alph1, self.alph2 = self.make_alph(k1), self.make_alph(k2)
 
     def crypt_b(self, bigr: str) -> str:
         """
-        Функция шифрования (расшифрования) биграмм
+        Функция шифрования биграмм
 
         :param bigr: шифруемая биграмма
         :returns: зашифрованная биграмма
         """
-        nums = [[self.alph1.index(bigr[0]) // 5, self.alph1.index(bigr[0]) % 5],
-                [self.alph2.index(bigr[1]) // 5, self.alph2.index(bigr[1]) % 5]]
-        """Массив для хранения строк и столбцов символов биграммы"""
-        # Проверяем, совпадают ли столбцы
-        if nums[0][1] == nums[1][1]:
-            # Если да, меняем строки
-            t = nums[0][0]
-            nums[0][0] = nums[1][0]
-            nums[1][0] = t
+        first_x, first_y = self.alph1.index(bigr[0]) // 5, self.alph1.index(bigr[0]) % 5
+        second_x, second_y = self.alph2.index(bigr[1]) // 5, self.alph2.index(bigr[1]) % 5
+        # Проверяем, совпадают ли строки
+        if first_x == second_x:
+            # Если да, взаимозаменяем номера столбцов, буквы берём из тех же таблиц, где находились исходные
+            first_y, second_y = second_y, first_x
+            return self.alph1[first_x * 5 + first_y] + self.alph2[second_x * 5 + second_y]
         else:
-            # Иначе взаимозаменяем номера столбцов
-            t = nums[0][1]
-            nums[0][1] = nums[1][1]
-            nums[1][1] = t
-        # Формируем зашифрованную биграмму
-        bigr = "".join([self.alph1[nums[0][0] * 5 + nums[0][1]], self.alph2[nums[1][0] * 5 + nums[1][1]]])
-        return bigr
-
-    def crypt(self, text: str) -> str:
+            # Иначе взаимозаменяем номера столбцов, буквы биграммы берём из разных таблиц
+            first_y, second_y = second_y, first_y
+            return self.alph2[first_x * 5 + first_y] + self.alph1[second_x * 5 + second_y]
+    
+    def decrypt_b(self, bigr: str) -> str:
         """
-        Функция шифрования (расшифрования) текста
-
-        :param text: шифруемый текст
-        :returns: зашифрованный текст
+        Функция для расшифрования биграмм
+        
+        :param bigr: шифруемая биграмма
+        :returns: зашифрованная биграмма
         """
-        text = self.replace(text)
-        if self.check_bad_lang(text):
-            print("Допускается использовать только русский алфавит!")
-            return
-        if len(text) % 2 == 1:
-            text += "ь"
-        ctext = ""
-        while len(ctext) < len(text):
-            ctext += self.crypt_b(text[len(ctext)] + text[len(ctext) + 1])
-        return ctext
-
-    """Алиас для расшифрования текста"""
-    decrypt = crypt
-    """Алиас для расшифрования биграмм"""
-    decrypt_b = crypt_b
+        # Сразу вычислим координаты так, как если бы у нас произошла замена из разных таблиц
+        first_x, first_y = self.alph2.index(bigr[0]) // 5, self.alph2.index(bigr[0]) % 5
+        second_x, second_y = self.alph1.index(bigr[1]) // 5, self.alph1.index(bigr[1]) % 5
+        # Поскольку номера строк у нас сохраняются, следует для начала проверить, не одинаковы ли они.
+        if first_x == second_x:
+            # Строки равны, значит, буквы брались из исходных таблиц. Просчитаем столбцы, сразу же их заменив
+            first_y, second_y = self.alph2.index(bigr[1]), self.alph1.index(bigr[0])
+            return self.alph1[first_x * 5 + first_y] + self.alph2[second_x * 5 + second_y]
+        else:
+            # Если нет, то заменяем столбцы
+            first_y, second_y = second_y, first_y
+            return self.alph1[first_x * 5 + first_y] + self.alph2[second_x * 5 + second_y]
