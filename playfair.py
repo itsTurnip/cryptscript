8ff2888dde4367b8190e606e775b52e050cd6a9e
itsTurnip <22935812+itsTurnip@users.noreply.github.com>
2020-10-22 22:06:34 +0500

    Фикс Плейфера

--- playfair.py
+++ playfair.py
@@ -1,21 +1,40 @@
 from .table import Table
 
-
 class Playfair(Table):
     """
     Классовая реализация шифра Плейфера
 
     :param key: Ключ закрытого алфавита
     """
+
     def __init__(self, key: str) -> str:
         key = self.replace(key)
         if self.check_bad_lang(key): 
-            print("Должны использоваться исключительно русские символы!")
-            del self
-            return
+            raise ValueError("Должны использоваться исключительно русские символы!")
         key = self.edit_key(key)
         self.alph = self.make_alph(key)
 
+    def crypt(self, text: str) -> str:
+        """
+        Функция шифрования текста
+
+        :param text: шифруемый текст
+        :returns: зашифрованный текст
+        """
+        text = self.replace(text)
+        if self.check_bad_lang(text):
+            raise ValueError()
+        if len(text) % 2 == 1:
+            text += 'ь'
+        ctext = ""
+        length = len(ctext)
+        while length < len(text):
+            if text[length] == text[length+1]:
+                text = text[:length+1] + 'ьь' + text[length+1:]
+            ctext += self.crypt_b(text[length:length + 2])
+            length = len(ctext)
+        return ctext
+    
     def crypt_b(self, bigr: str) -> str:
         """
         Функция шифрования биграмм
@@ -23,25 +42,23 @@ class Playfair(Table):
         :param bigr: Шифруемая биграмма
         :returns: Зашифрованная биграмма
         """
-        nums = [[self.alph.index(bigr[0]) // 5, self.alph.index(bigr[0]) % 5],
-                [self.alph.index(bigr[1]) // 5, self.alph.index(bigr[1]) % 5]]
+        first_x, first_y = self.alph.index(bigr[0]) // 5, self.alph.index(bigr[0]) % 5
+        second_x, second_y = self.alph.index(bigr[1]) // 5, self.alph.index(bigr[1]) % 5
         """Массив для хранения строк и столбцов символов биграммы"""
-        if nums[0][0] == nums[1][0]:
+        if first_x == second_x:
             # Проверяем, совпадают ли строки
             # Если да, то прибавляем к столбцу единицу
-            nums[0][1] = (nums[0][1] + 1) % 5
-            nums[1][1] = (nums[1][1] + 1) % 5
-        elif nums[0][1] == nums[1][1]:
+            first_y = (first_y + 1) % 5
+            second_y = (second_y + 1) % 5
+        elif first_y == second_y:
             # Проверяем совпадают ли столбцы
             # Если да, то прибавляем к строке единицу
-            nums[0][0] = (nums[0][0] + 1) % 5
-            nums[1][0] = (nums[1][0] + 1) % 5
+            first_x = (first_x + 1) % 6
+            second_x = (second_x + 1) % 6
         else:
             # Иначе взаимозаменяем номера столбцов
-            t = nums[0][1]
-            nums[0][1] = nums[1][1]
-            nums[1][1] = t
-        bigr = "".join([self.alph[nums[0][0] * 5 + nums[0][1]], self.alph[nums[1][0] * 5 + nums[1][1]]])
+            first_y, second_y = second_y, first_y
+        bigr = self.alph[first_x * 5 + first_y] + self.alph[second_x * 5 + second_y]
         return bigr
     
     def decrypt_b(self, bigr: str) -> str:
@@ -51,44 +68,24 @@ class Playfair(Table):
         :param bigr: Расшифруемая биграмма
         :returns: Расшифрованная биграмма
         """
-        nums = [[self.alph.index(bigr[0]) // 5, self.alph.index(bigr[0]) % 5],
-                [self.alph.index(bigr[1]) // 5, self.alph.index(bigr[1]) % 5]]
-        if nums[0][0] == nums[1][0]:
+        first_x, first_y = self.alph.index(bigr[0]) // 5, self.alph.index(bigr[0]) % 5
+        second_x, second_y = self.alph.index(bigr[1]) // 5, self.alph.index(bigr[1]) % 5
+        if first_x == second_x:
             # Проверяем, совпадают ли строки
             # Если да, то отнимаем от столбца единицу
-            nums[0][1] = (5 + nums[0][1] - 1) % 5
-            nums[1][1] = (5 + nums[1][1] - 1) % 5
-        elif nums[0][1] == nums[1][1]:
+            first_y = (first_y - 1) % 5
+            second_y = (second_y - 1) % 5
+        elif first_y == second_y:
             # Проверяем совпадают ли столбцы
             # Если да, то отнимаем от строки единицу
-            nums[0][0] = (5 + nums[0][0] - 1) % 5
-            nums[1][0] = (5 + nums[1][0] - 1) % 5
+            first_x = (first_x - 1) % 6
+            second_x = (second_x - 1) % 6
         else:
             # Иначе взаимозаменяем номера столбцов
-            t = nums[0][1]
-            nums[0][1] = nums[1][1]
-            nums[1][1] = t
-        bigr = "".join([self.alph[nums[0][0] * 5 + nums[0][1]], self.alph[nums[1][0] * 5 + nums[1][1]]])
+            first_y, second_y = second_y, first_y
+        bigr = self.alph[first_x * 5 + first_y] + self.alph[second_x * 5 + second_y]
         return bigr
 
-    def crypt(self, text: str) -> str:
-        """
-        Функция шифрования текста
-
-        :param text: Шифруемый текст
-        :returns: Зашифрованный текст
-        """
-        text = self.replace(text)
-        if self.check_bad_lang(text):
-            print("Допускается использовать только русский алфавит!")
-            return
-        if len(text) % 2 == 1:
-            text += "ь"
-        ctext = ""
-        while len(ctext) < len(text):
-            ctext += self.crypt_b(text[len(ctext)] + text[len(ctext) + 1])
-        return ctext
-    
     def decrypt(self, text: str) -> str:
         """
         Функция расшифрования текста
@@ -98,5 +95,5 @@ class Playfair(Table):
         """
         ctext = ""
         while len(ctext) < len(text):
-            ctext += self.decrypt_b(text[len(ctext)] + text[len(ctext) + 1])
+            ctext += self.decrypt_b(text[len(ctext):len(ctext) + 2])
         return ctext
